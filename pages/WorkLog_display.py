2f7093f610b03e992ecaaa501fbbb2d7be82f7d8
stwuiwmoc <stwuiwmoc5929@gmail.com>
2026-01-03 20:05:14 +0900

    画面上で選択した日付に基づいて工数実績を表示する機能を追加

--- pages/WorkLog_display.py
+++ pages/WorkLog_display.py
@@ -1,11 +1,10 @@
 import os
 import sys
-from datetime import datetime
 
 import streamlit as st
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import models.Task_definition as Task_def
+import services.B_WillDo_create as Output_B
 import services.E_WorkLog_formatting as Output_E
 from sidebar import task_view
 
@@ -13,16 +12,30 @@ if __name__ == "__main__":
     st.set_page_config(layout="wide")
     task_view.task_sidebar()
 
-    # データ処理
-    worklog_date = datetime(2026, 1, 2).date()
-    df_sum_subtask = Output_E.sum_df_each_subtask(worklog_date)
-    df_sum_order = Output_E.sum_df_each_order(df_sum_subtask)
-    df_display = Output_E.convert_df_for_display(df_sum_order)
+    col_left, col_right = st.columns([1, 1])
+    with col_left:
+        st.markdown("#### BJP入力用出力")
+    with col_right:
+        selected_date = st.date_input(
+            "日付を選択してください",
+            value=Output_B.get_without_today_latest_WillDO_date(),
+            key="willdo_date_input",
+            label_visibility="collapsed"
+        )
+    selected_str = selected_date.strftime("%y%m%d")
+    WorkLog_filename = os.path.join("data", "WorkLogs", f"工数実績{selected_str}.csv")
 
-    # 表示
-    st.markdown("#### オーダ別集計（切り捨て後）")
-    st.table(df_display)
+    if os.path.exists(WorkLog_filename):
+        # データ処理
+        df_sum_subtask = Output_E.sum_df_each_subtask(selected_date)
+        df_sum_order = Output_E.sum_df_each_order(df_sum_subtask)
+        df_display = Output_E.convert_df_for_display(df_sum_order)
 
-    st.markdown("#### サブタスク別集計")
-    st.data_editor(
-        df_sum_subtask, use_container_width=True)
+        # 表示
+        st.table(df_display)
+
+        st.markdown("#### サブタスク別集計")
+        st.data_editor(
+            df_sum_subtask, use_container_width=True)
+    else:
+        st.info("工数実績csv未作成です")
